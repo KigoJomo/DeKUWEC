249ff4c9cbe21ba1f976c767496c0779b0ecbd84
KigoJomo <kigojay684@gmail.com>
2023-11-06 22:09:58 +0300

    Sections

--- src/script.js
+++ src/script.js
@@ -18,29 +18,29 @@ function HandleScroll() {
   });
 
   // Update the background position based on the section index
-  switch (sectionIndex) {
-    case 0:
-      body.style.backgroundPosition = "top left";
-      break;
-    case 1:
-      body.style.backgroundPosition = "top right";
-      break;
-    case 2:
-      body.style.backgroundPosition = "bottom right";
-      break;
-    case 3:
-      body.style.backgroundPosition = "bottom left";
-      break;
-    case 4:
-      body.style.backgroundPosition = "center";
-      break;
-    case 5:
-      body.style.backgroundSize = "100vw";
-      break;
-    default:
-      body.style.backgroundPosition = "center";
-      body.style.backgroundSize = "cover";
-  }
+  // switch (sectionIndex) {
+  //   case 0:
+  //     body.style.backgroundPosition = "top left";
+  //     break;
+  //   case 1:
+  //     body.style.backgroundPosition = "top right";
+  //     break;
+  //   case 2:
+  //     body.style.backgroundPosition = "bottom right";
+  //     break;
+  //   case 3:
+  //     body.style.backgroundPosition = "bottom left";
+  //     break;
+  //   case 4:
+  //     body.style.backgroundPosition = "center";
+  //     break;
+  //   case 5:
+  //     body.style.backgroundSize = "100vw";
+  //     break;
+  //   default:
+  //     body.style.backgroundPosition = "center";
+  //     body.style.backgroundSize = "cover";
+  // }
 
   // Update the active nav link
   nav_links.forEach((link) => link.classList.remove("active"));
